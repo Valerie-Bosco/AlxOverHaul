0291854d931c22241d65a6cb93f2c79f3aee1a0f
Valery-AA <valeryatart@gmail.com>
2025-02-21 00:33:25 +0100

    v0.7.7

--- AlxOverHaul/__init__.py
+++ AlxOverHaul/__init__.py
@@ -14,7 +14,7 @@ bl_info = {
     "author": "Valerie Bosco[Valy Arhal]",
     "description": "",
     "warning": "[Heavly Under Development] And Subject To Substantial Changes",
-    "version": (0, 7, 6),
+    "version": (0, 7, 7),
     "blender": (4, 0, 0),
     "category": "3D View",
     "location": "[Ctrl Alt A] General Menu, [Shift Alt S] Pivot Menu, [Tab] Auto Mode Pie Menu",
